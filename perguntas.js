f0dcbb93ac2088133bf69f797d797acaf6608529
bibi5678 <140610937+bibi5678@users.noreply.github.com>
2024-10-29 08:49:35 -0300

    vfd

---
+++ perguntas.js
@@ -0,0 +1,34 @@
+criaCartao(
+    'Biologia',
+    'O que a mitocôndria?',
+    ' respiração'
+)
+criaCartao(
+    'Historia',
+    'Quais eram os países da tríplice entente?',
+    'França, Inglaterra e Rússia'
+)
+criaCartao(
+    'Química',
+    'O que é uma cadeia de carbono? ',
+    'São as estruturas básicas do compostos orgânicos' 
+)
+criaCartao(
+    'LÍNGUA PORTUGUESA',
+    'O que é uma reportagem?',
+    'É um gênero textual com o objetivo de passar informações e conteúdo para o público'
+)
+criaCartao(
+    'EMPREENDEDORISMO',
+    'Qual é a idade mínima para ingressar em um programa de jovem aprendiz?',
+    '14 anos'
+)
+criaCartao(
+    'BIOTECNOLOGIA',
+    'Qual a primeira lei de Mendel?',
+    'Princípio da Segregação dos Caracteres'
+)
+criaCartao(
+    'SOCIOLOGIA'
+    ''
+)
